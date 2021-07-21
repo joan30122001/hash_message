5a2e4857369e661b1dfcb778a464b25c0dc0a6db
joan30122001 <ngounouloic675@gmail.com>
2021-07-21 06:53:52 +0200

    ajout du template et du code python

--- Hash_Message/settings.py
+++ Hash_Message/settings.py
@@ -119,3 +119,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+MEDIA_ROOT = os.path.join(BASE_DIR, "media")
+# STATIC_ROOT = '/absolute/path/to/static/dir/in/doc_root/'
+MEDIA_URL = '/media/'
--- hash/views.py
+++ hash/views.py
@@ -1,3 +1,36 @@
+from hash.utils.HashFile import HashFile
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    # hash = HashFile()
+    # h = int(input(f"Entrez une valeur conprise {hash.q} et {hash.p} : "))
+    # hash.parameter_generation(h)
+    # hash.per_user_key()
+
+    # hash.name=input("Enter the name of document to sign: ")
+    # print(hash.signature())
+
+    # print("r(Component of signature) is: ",hash.r)
+    # print("k(Randomly chosen number) is: ",hash.k)
+    # print("s(Component of signature) is: ",hash.s)
+
+    # print()
+    # print(hash.verification())
+    return render(request, "index.html")
+
+
+    hash = HashFile()
+    h = int(input(f"Entrez une valeur conprise {hash.q} et {hash.p} : "))
+    hash.parameter_generation(h)
+    hash.per_user_key()
+
+    hash.name=input("Enter the name of document to sign: ")
+    print(hash.signature())
+
+    print("r(Component of signature) is: ",hash.r)
+    print("k(Randomly chosen number) is: ",hash.k)
+    print("s(Component of signature) is: ",hash.s)
+
+    print()
+    print(hash.verification())
