42d243f2223a8023aad2536757fa60fa6f4829ff
joan30122001 <64334679+joan30122001@users.noreply.github.com>
2021-07-24 17:21:15 +0200

    Revert "Master"

--- Hash_Message/settings.py
+++ Hash_Message/settings.py
@@ -119,6 +119,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
-MEDIA_ROOT = os.path.join(BASE_DIR, "media")
-# STATIC_ROOT = '/absolute/path/to/static/dir/in/doc_root/'
-MEDIA_URL = '/media/'
